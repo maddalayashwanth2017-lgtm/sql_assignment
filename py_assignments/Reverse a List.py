092b40cdecb09d18e5df11367b833e54affb2218
maddalayashwanth2017-lgtm <maddalayashwanth2017@gmail.com>
2026-04-02 16:00:55 +0530

    Delete py_assignments/Reverse a List.py

--- py_assignments/Reverse a List.py
+++
@@ -1,5 +0,0 @@
-# Given the list [10, 20, 30, 40, 50], write a program using a for loop to print the list elements in reverse order.
-
-num=[10,20,30,40,50]
-for i in range (len(num)-1,-1,-1):
-   print(num[i],end=" ")
